b0722453bdadb9ddd7ffc9251c2a7d9b195009a3
Sunaina <ssr8921@gmail.com>
2018-05-31 22:40:37 +0530

    adkf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -47,6 +48,7 @@ const appRoutes: Routes =[
     BrowserModule,
     HttpClientModule,
     HttpModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [ServiceService,ElasticsearchService],
