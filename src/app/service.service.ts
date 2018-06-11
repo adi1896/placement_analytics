fd51bf366d52407ff06da768cc9aa86c812bf9e3
adi1896 <aditya@woir.in>
2018-06-11 20:34:53 +0530

    oneclick is wording and added service

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpModule, Http, Response, RequestOptions, Headers, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ServiceService {
+  clgname:any;
 
-  constructor(private http:HttpClient) { 
+  constructor(private http:Http) { 
     console.log('service is running ...');
     let id=1;
     let state='Madhya Pradesh';
@@ -21,4 +22,110 @@ export class ServiceService {
       let country='India';
       return this.http.get('http://192.168.0.2:8182/html/getdata.php?id='+id+'&country='+country);
     }
+
+    elasticPost(clgname = null, country = "India", state = null, city = null) {
+      this.clgname='*'+clgname+'*'
+      console.log(this.clgname)
+      let headers = new Headers({ 'Content-Type': "application/x-www-form-urlencoded" });
+      let search = new URLSearchParams();
+    
+      let obj = {
+        "query": {
+          "bool": {
+            "must": [
+              {
+                "query_string": {
+                  "analyze_wildcard": false,
+                  "query": "*"
+                }
+              },
+              {
+                "query_string": {
+                  "analyze_wildcard": false,
+                  "query": "*"
+                }
+              },
+              {
+                "range": {
+                  "Date": {
+                    "gte": 137071994078,
+                    "lte": 1528486340786,
+                    "format": "epoch_millis"
+                  }
+                }
+              },
+              {
+                "match": {
+                  "State": state
+                }
+              },
+              {
+                "match": {
+                  "City": city
+                }
+              },
+              {
+                "wildcard": {
+                  "Full_Name": this.clgname
+                }
+              }
+            ],
+            "must_not": []
+          }
+        },
+        "size": 0,
+        "_source": {
+          "excludes": []
+        },
+        "aggs": {
+          "2": {
+            "terms": {
+              "field": "Full_Name",
+              "size": 281,
+              "order": {
+                "_count": "desc"
+              }
+            },
+            "aggs": {
+              "3": {
+                "terms": {
+                  "field": "Batch",
+                  "size": 25,
+                  "order": {
+                    "_count": "desc"
+                  }
+                },
+                "aggs": {
+                  "4": {
+                    "terms": {
+                      "field": "Branch",
+                      "size": 10,
+                      "order": {
+                        "_count": "desc"
+                      }
+                    },
+                    "aggs": {
+                      "5": {
+                        "terms": {
+                          "field": "Name_of_the_company .keyword",
+                          "size": 1000,
+                          "order": {
+                            "_count": "desc"
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      };
+      return this.http.post('http://192.168.0.3:9200/jobs1/_search', JSON.stringify(obj), { headers: headers })
+      .map(res => res.json()) ;
+  
+    }
+
+
  }
